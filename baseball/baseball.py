431e6947c6b1d91348105d5ac72bfde7f12080a4
yonghyeokrhee <joshuaisland@gmail.com>
2020-08-08 09:29:13 +0900

    home

---
+++ baseball/baseball.py
@@ -0,0 +1,20 @@
+def solutions():
+
+    guess = str(input[0][0])
+    if guess[0] == strike_letter:
+        guess[1] or guess[2] is ball_letter
+
+    if guess[1] == strikie_letter:
+        guess[0] or guess[2] is ball_letter
+
+
+    return possibles
+
+
+
+
+input = [[123, 1, 1], [356, 1, 0], [327, 2, 0], [489, 0, 1]]
+solutions()
+
+# input [[123, 1, 1], [356, 1, 0], [327, 2, 0], [489, 0, 1]]
+# return 2
