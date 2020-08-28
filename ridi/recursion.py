64738a9e114be5f837c3cbb1f680c9d6dc152159
yonghyeokrhee <joshuaisland@gmail.com>
2020-08-28 23:17:11 +0900

    len(arr) == 3 passed

--- ridi/recursion.py
+++ ridi/recursion.py
@@ -10,30 +10,30 @@ import random
 
 
 def solution(arr):
-    answer = []
+    n = len(arr)
+    if n == 1:
+        return arr
+    elif n == 2:
+        arr_r = arr[::-1]
+        assert isinstance(arr_r, list)
+        return arr, arr_r
+    else:
+        for i in range(n - 1):
+            lefty = arr[:i + 1]
+            righty = arr[i + 1:n]
 
-    def reverse(arr):
-        n = len(arr)
-        if n == 1:
-            return arr
-        elif n == 2:
-            arr_r = arr[::-1]
-            print([arr,arr_r])
-            return [arr, arr_r]
-        else:
-            for i in range(n-1):
-                lefty = arr[:i+1]
-                righty = arr[i+1:n]
-                #print(lefty)
-                #print(righty)
-                for elem in reverse(righty):
-                    print(elem)
-                    print('combine:{}'.format(elem + lefty))
-                #print('combine:{},{}'.format(reverse(righty),reverse(lefty)))
-                #answer.append(reverse(lefty)[0])
-    reverse(arr)
+            for left in solution(lefty):
+                if isinstance(left, int):
+                    for right in solution(righty):
+                        print('combine:{}'.format([left]+right))
+                else:
+                    for right in solution(righty):
+                        print('combine:{}'.format(left+[right]))
+            # print('combine:{},{}'.format(reverse(righty),reverse(lefty)))
+            # answer.append(reverse(lefty)[0])
     return answer
 
 
-solution([1, 2,3])
+answer = []
+solution([1, 2, 3])
 # solution([3,1,2])
