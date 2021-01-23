74f35487d0046b63f662faeaab5b86ab5b2dfb56
kanto <joshuaisland@gmail.com>
2021-01-23 16:50:48 +0900

    all fixed

--- algorithm-book/trappingwaters.py
+++ algorithm-book/trappingwaters.py
@@ -1,4 +1,6 @@
 import copy
+import time
+start_time = time.time()
 
 # double count for i = 5 and i = 7 because they have same condition
 # unnecessary second while loop after first loop condition not met - waste of time
@@ -6,7 +8,7 @@ def trapping_water(inputs):
     vol = 0
     record = []
     max = len(inputs)
-    for i in range(len(inputs)-1):
+    for i in range(len(inputs) - 1):
         if i == 0:
             continue
         # running loop
@@ -18,25 +20,30 @@ def trapping_water(inputs):
             if k == -1:
                 break
         # end for k loop
-
-        # start of j loop
-        while inputs[i] >= inputs[j] and j <= len(inputs):
-            j += 1
-            if j == max:
-                break
-        if not (k > 0 and j < len(inputs)):
-            continue
-        else:
-            if k not in record:
-                left_bar = inputs[k]
-                right_bar = inputs[j]
-                vol += (j- k -1) * (min(right_bar, left_bar) - inputs[i])
-                record.append(k)
+        # give a condition only if k > 0
+        if k != -1:
+            # start of j loop
+            while inputs[i] >= inputs[j] and j <= len(inputs):
+                j += 1
+                if j == max:
+                    break
+            if not (k > 0 and j < len(inputs)):
+                continue
             else:
-                pass
+                if k not in record:
+                    left_bar = inputs[k]
+                    right_bar = inputs[j]
+                    vol += (j - k - 1) * (min(right_bar, left_bar) - inputs[i])
+                    record.append(k)
+                else:
+                    pass
+        else:
+            print("k was -1")
+            pass
     print(vol)
     return vol
 
 
 inputs = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
 trapping_water(inputs)
+print("--- %s seconds ---" % (time.time() - start_time))
